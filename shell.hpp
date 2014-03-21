dbf6e6cd6011c50bf1ea707185a0fa00b0b9f7f7
manasij7479 <manasij7479@gmail.com>
2014-03-21 22:27:46 +0530

    shell abstraction

---
+++ shell.hpp
@@ -0,0 +1,19 @@
+#ifndef MM_SHELL_HPP
+#define MM_SHELL_HPP
+#include <iostream>
+#include "sqlitewrapper.hpp"
+namespace mm
+{
+	class Shell
+	{
+	public:
+		Shell(Database& db_):db(db_){};
+		Database& getDb(){return db;}
+		virtual void operator()()=0;
+		virtual ~Shell(){};
+	private:
+		Database& db;
+	};
+	
+}
+#endif
